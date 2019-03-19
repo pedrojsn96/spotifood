48857f580e6824eccb175fbbfbf14a10e035abb5
Pedro Neto <pedroneto@Pedros-MacBook-Air.local>
2019-03-19 15:45:54 -0300

    removing redirect

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -41,8 +41,8 @@ export default class Login extends Component {
 		localStorage.setItem('@SpotiFood:expires_in', expires_in);
 
 		// window.location.replace('http://localhost:3000/home');
-		// window.location.replace('http://spotifood-p.herokuapp.com/home');
-		<Redirect to="/home" push={true} />;
+		window.location.replace('http://spotifood-p.herokuapp.com/home');
+		// <Redirect to="/home" push={true} />;
 	};
 
 	componentDidMount() {
